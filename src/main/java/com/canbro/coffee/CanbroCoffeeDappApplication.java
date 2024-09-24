980c9abe908521605d53767b9857d42d6865dab2
zhut <1339787050@qq.com>
2024-09-24 11:55:59 +0800

    init

---
+++ src/main/java/com/canbro/coffee/CanbroCoffeeDappApplication.java
@@ -0,0 +1,13 @@
+package com.canbro.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanbroCoffeeDappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanbroCoffeeDappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/canbro/coffee/CanbroCoffeeDappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canbro.coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanbroCoffeeDappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
